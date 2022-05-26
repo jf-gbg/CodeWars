6ec2cd1a0fc180314fbbdc32a7a7fb23e8970cf3
jf-gbg <malmmlam@outlook.com>
2022-05-26 18:00:09 +0200

    switch to full C# console app

---
+++ Gravity_Flip/Gravity_Flip/Gravity_Flip/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gravity_Flip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
