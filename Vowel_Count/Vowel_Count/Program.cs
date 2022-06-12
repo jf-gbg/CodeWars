f68757008540c57819f926dd0cf9a1fe320d0574
Dev <Dev@MacBook-Pro.local>
2022-06-12 08:40:14 +0200

    add new problem Vowel_Count

---
+++ Vowel_Count/Vowel_Count/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+
+        // Your code here
+
+        return vowelCount;
+    }
+}
+
+namespace Vowel_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
